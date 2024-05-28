3961a6815dea1892c875c17fb34f0615d0a75140
Kristiamnx <rivaskristopher24@gmail.com>
2024-05-28 12:51:20 -0500

    users

--- src/main/java/riwi/simulacro_SpringBoot/infrastructure/abstrac_services/IUserService.java
+++ src/main/java/riwi/simulacro_SpringBoot/infrastructure/abstrac_services/IUserService.java
@@ -5,5 +5,5 @@ import riwi.simulacro_SpringBoot.api.dto.responses.UserResponse;
 import riwi.simulacro_SpringBoot.infrastructure.abstrac_services.CrudService;
 
 public interface IUserService extends CrudService<UserRequest, UserResponse,Long> {
-    public Long FindById(Long id );
+
 }
--- src/main/java/riwi/simulacro_SpringBoot/infrastructure/services/UserService.java
+++ src/main/java/riwi/simulacro_SpringBoot/infrastructure/services/UserService.java
@@ -1,23 +1,33 @@
 package riwi.simulacro_SpringBoot.infrastructure.services;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import riwi.simulacro_SpringBoot.api.dto.requests.UserRequest;
 import riwi.simulacro_SpringBoot.api.dto.responses.UserResponse;
+import riwi.simulacro_SpringBoot.domain.entities.User;
+import riwi.simulacro_SpringBoot.domain.repositories.UserRepository;
 import riwi.simulacro_SpringBoot.infrastructure.abstrac_services.IUserService;
+import riwi.simulacro_SpringBoot.util.exceptions.IdNotFoundException;
+
 @Service
 @AllArgsConstructor
 public class UserService implements IUserService {
+    @Autowired
+    private  final UserRepository userRepository;
     @Override
     public UserResponse create(UserRequest request) {
-        return null;
+        User user = this.requestToUser(request,new User());
+        return this.entityResponse(this.userRepository.save(user));
     }
 
     @Override
     public UserResponse get(Long aLong) {
-        return null;
+        return ;
     }
+
 
     @Override
     public UserResponse update(UserRequest request, Long aLong) {
@@ -26,7 +36,8 @@ public class UserService implements IUserService {
 
     @Override
     public void delete(Long aLong) {
-
+    User user = this.find(aLong);
+    this.userRepository.delete(user);
     }
 
     @Override
@@ -34,8 +45,20 @@ public class UserService implements IUserService {
         return null;
     }
 
-    @Override
-    public Long FindById(Long id) {
-        return null;
+
+    private User find(Long id){
+        return this.userRepository.findById(id).orElseThrow(()->new IdNotFoundException("User"));
     }
+    private UserResponse entityResponse(User entity){
+        UserResponse response = new UserResponse();
+        BeanUtils.copyProperties(entity,response);
+        return response;
+    }
+    private User requestToUser (UserRequest request,User user){
+        BeanUtils.copyProperties(request,user);
+        return user;
+    }
+
+
+
 }
